8d52a4c7835e51272788c985b00ca82ca125a2da
scroutch <cecile.duhain@gmail.com>
2022-09-06 14:19:12 +0200

    html and css for header done

--- index.php
+++ index.php
@@ -13,14 +13,23 @@ include('bdd.php');
     <link rel="stylesheet" href="./assets/bootstrap-5.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="./assets/fontawesome-free-6.2.0-web/css/all.css">
     <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Moon+Dance&family=Moulpali&display=swap" rel="stylesheet">
     <script defer src="./assets/fontawesome-free-6.2.0-web/js/all.js"></script>
     <title>Restaurant aqua</title>
 </head>
 
 <body>
     <header>
-        <h1>Aqua</h1>
-        <i class="fa-solid fa-bars"></i>
+        <nav>
+            <h1>Aqua</h1>
+            <i class="fa-solid fa-bars"></i>
+        </nav>
+        <div class="header-cta">
+            <p>"Venez goûter nos produits de la mer"</p>
+            <a href="#">nous découvrir</a>
+        </div>
     </header>
     <?php
 
