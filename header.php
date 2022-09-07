e67b033464edb90a9eea8059bcbc74780b6fa42c
scroutch <cecile.duhain@gmail.com>
2022-09-07 11:25:18 +0200

    added second header for other page

---
+++ header.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <h1>Aqua</h1>
+        <i class="fa-solid fa-bars"></i>
+    </nav>
+    <div class="header-cta">
+        <p>"Venez goûter nos produits de la mer"</p>
+        <a href="#">nous découvrir</a>
+    </div>
+</header>
---
+++ header2.php
@@ -0,0 +1,6 @@
+<header>
+    <nav class="nav">
+        <h1>Aqua</h1>
+        <i class="fa-solid fa-bars"></i>
+    </nav>
+</header>
