4f38618f2a30311ea2a6a4960ee93b284c4f4dfd
scroutch <cecile.duhain@gmail.com>
2022-09-07 16:13:28 +0200

    Add cards for dishes in home.php

---
+++ home.php
@@ -0,0 +1,65 @@
+<div id="presentation">
+    <div class="presentation-carousel">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="./assets/img/carousel/resto.jpg" class="d-block w-100" alt="restaurant">
+                </div>
+                <div class="carousel-item">
+                <img src="./assets/img/carousel/chef.jpg" class="d-block w-100" alt="chef cuisinier">
+                </div>
+                <div class="carousel-item">
+                <img src="./assets/img/carousel/poissonSurAssiette.jpg" class="d-block w-100" alt="assiette de poisson">
+                </div>
+                <div class="carousel-item">
+                <img src="./assets/img/carousel/cuistot.jpg" class="d-block w-100" alt="cuisinier">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+    <div class="presentation-text">
+        <h2>Présentation</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti accusantium ad nostrum sed quasi aliquam quis et labore voluptates doloremque, sit nam voluptatum, voluptatem blanditiis. Facilis deleniti exercitationem mollitia necessitatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti accusantium ad nostrum sed quasi aliquam quis et labore voluptates doloremque, sit nam voluptatum, voluptatem blanditiis.</p>
+        <a href="#" class="button">Lire la suite ...</a>
+    </div>
+</div>
+<div id="menuCards">
+    <div class="entrees">
+        <div class="card">
+            <img src="./assets/img/stjacque.jpg" class="card-img-top" alt="saint jacques">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    <div class="plats">
+        <div class="card">
+            <img src="./assets/img/moule.jpg" class="card-img-top" alt="moules">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    <div class="desserts">
+        <div class="card">
+            <img src="./assets/img/glace.jpg" class="card-img-top" alt="glaces">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</div>
