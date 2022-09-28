4493bfbded161e99985eacf501fc403dcc4b3964
scroutch <cecile.duhain@gmail.com>
2022-09-28 09:44:45 +0200

    modify header2

--- pages/header2.php
+++ pages/header2.php
@@ -1,15 +1,14 @@
 <header>
-    <nav class="nav">
+    <nav>
         <h1>Aqua</h1>
-
-        <div class="menu-right">
-            <ul class="itemsmenu" id="menu-navigation">
-                <li class="menu-item"><a href="index.php?page=1">Accueil</a></li>
-                <li class="menu-item"><a href="index.php?page=2">Notre carte</a></li>
-                <li class="menu-item"><a href="index.php?page=3">Devis traiteur</a></li>
-                <li class="menu-item"><a href="index.php?page=4">Contact</a></li>
-                <li class="menu-item"><a href="index.php?page=5">Connexion</a></li>
-            </ul>
-        </div>
+        <ul class="itemsmenu" id="menu-navigation">
+            <li class="menu-item"><a href="index.php?page=1">Accueil</a></li>
+            <li class="menu-item">
+                <a href="index.php?page=2">Notre carte</a>
+            </li>
+            <li class="menu-item"><a href="index.php?page=3">Devis traiteur</a></li>
+            <li class="menu-item"><a href="index.php?page=4">Contact</a></li>
+            <li class="menu-item"><a href="index.php?page=5">Connexion</a></li>
+        </ul>
     </nav>
 </header>
