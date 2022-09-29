c6cf83af605cc1b6cf304636ff7449523a0956b3
scroutch <cecile.duhain@gmail.com>
2022-09-29 16:28:38 +0200

    Add img and file target_inscription

--- pages/header.php
+++ pages/header.php
@@ -5,10 +5,13 @@
         <div class="menu-right">
             <ul class="itemsmenu" id="menu-navigation">
                 <li class="menu-item"><a href="index.php?page=1">Accueil</a></li>
-                <li class="menu-item"><a href="index.php?page=2">Notre carte</a></li>
+                <li class="menu-item">
+                    <a href="index.php?page=2">Notre carte</a>
+                </li>
                 <li class="menu-item"><a href="index.php?page=3">Devis traiteur</a></li>
                 <li class="menu-item"><a href="index.php?page=4">Contact</a></li>
                 <li class="menu-item"><a href="index.php?page=5">Connexion</a></li>
+                <li class="menu-item"><a href="index.php?page=5">Inscription</a></li>
             </ul>
         </div>
     </nav>
