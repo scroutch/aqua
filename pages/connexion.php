cd68270dd96bb2a6e69596b6ae64d4107c6a1c2e
scroutch <cecile.duhain@gmail.com>
2022-10-02 00:08:51 +0200

    new html css for connect page

--- pages/connexion.php
+++ pages/connexion.php
@@ -3,29 +3,12 @@
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                    <div class="card shadow-2-strong" style="border-radius: 1rem;">
-                        <div class="card-body p-5 text-center">
-
-                            <h3 class="mb-5">Se connecter</h3>
-
-                            <div class="form-outline mb-4">
-                            <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
-                            <label class="form-label" for="typeEmailX-2">Email</label>
-                            </div>
-
-                            <div class="form-outline mb-4">
-                            <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
-                            <label class="form-label" for="typePasswordX-2">Mot de passe</label>
-                            </div>
-
-                            <div class="form-check d-flex justify-content-start mb-4">
-                            <input class="form-check-input" type="checkbox" value=" " id="form1Example3" />
-                            <label class="form-check-label" for="form1Example3"> Se souvenir du mot de passe </label>
-                            </div>
-
-                            <button class="btn button btn-lg btn-block" type="submit">Se connecter</button>
-                        </div>
-                    </div>
+                    <h3>Se connecter</h3>
+                    <form action="/target_connexion.php" method="post" id="form_connect">
+                        <input type="email" name="email" id="" placeholder="email">
+                        <input type="password" name="password" id="" placeholder="mot de passe">
+                        <button class="button" type="submit">Se connecter</button>
+                    </form>
                 </div>
             </div>
         </div>
